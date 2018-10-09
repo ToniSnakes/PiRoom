2dcbfe0ea3459829dfdf22eced9c4f662601fc1e
Robin Kock <robin@kock-hamburg.de>
2018-10-09 17:39:04 +0200

    flowchart

--- src/server/paf.h
+++ src/server/paf.h
@@ -6,6 +6,8 @@ paf_t* paf_init(
     size_t planSize,
     void (*handleStep)(const char* message, void* plan),
     void (*createPlan)(const char* message, void* plan));
+void paf_free(paf_t** paf);
+
 void paf_newMessage(paf_t* paf, char* message, void* planInit);
 void paf_dispatch(paf_t* paf);
 void paf_finishStep(paf_t* paf, void* plan);
